91a75b3e0974adabcebba15cd178487258d8f8ba
Hugh Harvey <69077999+DrHughHarvey@users.noreply.github.com>
2020-08-01 18:33:12 +0100

    Update run_blank_example.py

--- examples/run_blank_example.py
+++ examples/run_blank_example.py
@@ -10,14 +10,18 @@ from api import demo_web_app
 # Construct GPT object and show some examples
 gpt = GPT(engine="davinci", temperature=0.5, max_tokens=100)
 
-gpt.add_example(Example("Who are you?", "I'm an example."))
+gpt.add_example(Example("There are several hyerechoic calcific foci within the gallbladder with significant posterior acoustic shadowing in keeping with cholecystolithiasis", "There are several dense objects typical of gallstones within the gallbladder"))
+gpt.add_example(Example("What are you?", "I'm an example."))
+gpt.add_example(Example("What are you?", "I'm an example."))
+gpt.add_example(Example("What are you?", "I'm an example."))
+gpt.add_example(Example("What are you?", "I'm an example."))
 gpt.add_example(Example("What are you?", "I'm an example."))
 
 # Define UI configuration
 config = UIConfig(
-    description="Prompt",
-    button_text="Result",
-    placeholder="Where are you?",
+    description="Translate clinical radiology reports into lay versions",
+    button_text="Go",
+    placeholder="Lay version",
     show_example_form=True,
 )
 
